6c5166184c2f8f3c52a2f9ee66a494130dce5e5b
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-02-01 20:59:01 -0300

    feat: separate modules

---
+++ src/builder/mysql/select.rs
@@ -0,0 +1,68 @@
+pub struct Select {
+    distinct: bool,
+    from: String,
+    limit: u32,
+    columns: Vec<String>,
+}
+
+#[allow(dead_code)]
+impl Select {
+    pub fn new() -> Select {
+        Select {
+            distinct: false,
+            from: String::new(),
+            limit: 0,
+            columns: Vec::new(),
+        }
+    }
+
+    pub fn distinct(&mut self) -> &mut Select {
+        self.distinct = true;
+        self
+    }
+
+    pub fn from(&mut self, from: &str) -> &mut Select {
+        self.from = from.to_string();
+        self
+    }
+
+    pub fn columns(&mut self, columns: Vec<&str>) -> &mut Select {
+        self.columns = columns.iter().map(|s| s.to_string()).collect();
+        self
+    }
+
+    pub fn limit(&mut self, limit: u32) {
+        self.limit = limit;
+    }
+
+    pub fn build(&self) -> String {
+        let mut statement = "SELECT ".to_string();
+
+        if self.distinct {
+            statement.push_str("DISTINCT ");
+        }
+
+        if self.columns.len() > 0 {
+            for (index, col) in self.columns.iter().enumerate() {
+                if index == self.columns.len() - 1 {
+                    statement.push_str(&format!("{} ", &col));
+                    continue;
+                }
+                statement.push_str(&format!("{}, ", &col));
+            }
+        } else {
+            statement.push_str("* ");
+        }
+
+        if self.from.len() > 0 {
+            statement.push_str(&format!("FROM {} ", self.from));
+        }
+
+        if self.limit > 0 {
+            statement.push_str(&format!("LIMIT {}", self.limit));
+        }
+
+        statement = statement.trim().to_string() + ";";
+        statement
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use sql_builder::mysql::Select;
+mod builder;
+
+use builder::mysql::select::Select;
 
 fn main() {
     let mut select = Select::new();
@@ -8,78 +10,4 @@ fn main() {
         .columns(vec!["name", "age", "email"]);
 
     println!("{}", select.build());
-}
-
-pub mod sql_builder {
-
-    pub mod mysql {
-
-        pub struct Select {
-            distinct: bool,
-            from: String,
-            limit: u32,
-            columns: Vec<String>,
-        }
-
-        impl Select {
-            pub fn new() -> Select {
-                Select {
-                    distinct: false,
-                    from: String::new(),
-                    limit: 0,
-                    columns: Vec::new(),
-                }
-            }
-
-            pub fn distinct(&mut self) -> &mut Select {
-                self.distinct = true;
-                self
-            }
-
-            pub fn from(&mut self, from: &str) -> &mut Select {
-                self.from = from.to_string();
-                self
-            }
-
-            pub fn columns(&mut self, columns: Vec<&str>) -> &mut Select {
-                self.columns = columns.iter().map(|s| s.to_string()).collect();
-                self
-            }
-
-            pub fn limit(&mut self, limit: u32) {
-                self.limit = limit;
-            }
-
-            pub fn build(&self) -> String {
-                let mut statement = "SELECT ".to_string();
-
-                if self.distinct {
-                    statement.push_str("DISTINCT ");
-                }
-
-                if self.columns.len() > 0 {
-                    for (index, col) in self.columns.iter().enumerate() {
-                        if index == self.columns.len() - 1 {
-                            statement.push_str(&format!("{} ", &col));
-                            continue;
-                        }
-                        statement.push_str(&format!("{}, ", &col));
-                    }
-                } else {
-                    statement.push_str("* ");
-                }
-
-                if self.from.len() > 0 {
-                    statement.push_str(&format!("FROM {} ", self.from));
-                }
-
-                if self.limit > 0 {
-                    statement.push_str(&format!("LIMIT {}", self.limit));
-                }
-
-                statement = statement.trim().to_string() + ";";
-                statement
-            }
-        }
-    }
 }
