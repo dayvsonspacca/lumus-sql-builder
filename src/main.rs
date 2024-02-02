1497c57e23f9c25c0b22106004a79d4babd12723
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-02-02 08:33:28 -0300

    chore: select example

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,13 @@ fn main() {
 
     where_.equal_to("name", "2")
           .not_equal_to("email", "spacca.dayvson@gmail.com")
-          .greater_than("age", "2");
+          .greater_than("age", "2")
+          .greater_than_equal("age", "2")
+          .less_than("salary", "230.00")
+          .less_than_equal("age", "25");
 
     select
-        .columns("name, age, email")
+        .columns("name, age, email, salary")
         .from("users_tb")
         .where_(where_)
         .group("name")
