3f4c678fe9d93cd8351460ba60849ec1b8bb1e04
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-03-02 00:35:55 -0300

    feat: add basic create statement.

---
+++ src/mysql/create.rs
@@ -0,0 +1,23 @@
+pub struct Create<'a> {
+    columns: &'a str,
+    table: &'a str,
+}
+
+#[allow(dead_code)]
+impl<'a> Create<'a> {
+    pub fn new(table: &'a str) -> Create<'a> {
+        Create { table, columns: "" }
+    }
+
+    pub fn columns(&mut self, columns: &'a str) -> &Create<'a> {
+        self.columns = columns;
+        self
+    }
+
+    pub fn build(&self) -> String {
+        let mut statement = "CREATE TABLE IF NOT EXISTS ".to_string();
+        statement.push_str(&format!("{} ", self.table));
+        statement.push_str(&format!("({});", self.columns));
+        statement
+    }
+}
--- src/mysql/mod.rs
+++ src/mysql/mod.rs
@@ -1,3 +1,4 @@
+pub mod create;
 pub mod delete;
 pub mod insert;
 pub mod join;
