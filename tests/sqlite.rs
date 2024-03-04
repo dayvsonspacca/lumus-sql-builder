5662d22a43800d2c673ce6e5c520331956a85bbf
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-03-04 17:20:15 -0300

    test: add tests from sqlite create statements

---
+++ tests/sqlite.rs
@@ -0,0 +1,38 @@
+use lumus_sql_builder::sqlite::create::{Column, CreateTable};
+
+#[test]
+fn test_create_table() {
+    let create = CreateTable::new(
+        "example_table",
+        false,
+        vec![
+            Column::new("id").literal("INT"),
+            Column::new("name").literal("VARCHAR(255)"),
+            Column::new("age").literal("INT"),
+        ],
+    )
+    .build();
+
+    let expected_sql = "CREATE TABLE example_table (id INT, name VARCHAR(255), age INT);";
+
+    assert_eq!(create, expected_sql);
+}
+
+#[test]
+fn test_create_table_if_not_exists() {
+    let create = CreateTable::new(
+        "example_table",
+        true,
+        vec![
+            Column::new("id").literal("INT"),
+            Column::new("name").literal("VARCHAR(255)"),
+            Column::new("age").literal("INT"),
+        ],
+    )
+    .build();
+
+    let expected_sql =
+        "CREATE TABLE IF NOT EXISTS example_table (id INT, name VARCHAR(255), age INT);";
+
+    assert_eq!(create, expected_sql);
+}
