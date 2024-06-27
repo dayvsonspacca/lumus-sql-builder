dd204ab219d99c4ad563bfb60c196e3a5d493fd0
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-06-27 16:06:05 -0300

    docs: add lib documetation + README

--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 
 ## About project
 
-The project is an SQL query builder that allows you to create complex SQL queries programmatically and intuitively. It supports a variety of SQL operations, including column selection, joins, WHERE clauses, grouping, sorting, and much more.
+Lumus SQL Builder is a Rust library that allows you to programmatically and intuitively construct complex SQL queries. It supports a variety of SQL operations, including column selection, joins, WHERE clauses, grouping, sorting, and more.
 
-Here is an example of how the code can be used to build an SQL query:
+## Features
+
+-   Table creation
+-   Data insertion
+-   Data selection with support for DISTINCT, GROUP BY, ORDER BY, LIMIT, and OFFSET
+-   Complex WHERE clauses
+
+## Example Usage
+
+### Creating a Table
 
 ```rust
 use lumus_sql_builder::sqlite::{CreateTable, Column};
@@ -22,17 +31,65 @@ fn main() {
 
     println!("{}", create_table.build().unwrap());
 }
-
 ```
-# Output: 
+
+### Output
+
 ```sql
 CREATE TABLE employees (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     age INTEGER NOT NULL,
     department TEXT DEFAULT 'Undefined',
-    salary REAL
+    salary REAL,
     hired_date DATETIME,
     manager_id INTEGER
 );
+```
+
+### Inserting Data
+
+```rust
+use lumus_sql_builder::sqlite::Insert;
+
+fn main() {
+    let insert = Insert::new("employees").values(vec![
+        ("name", "John"),
+        ("age", "30"),
+        ("department", "IT"),
+        ("salary", "5000.00"),
+        ("hired_date", "2024-03-20"),
+        ("manager_id", "1")
+    ]);
+
+    println!("{}", insert.build().unwrap());
+}
+```
+
+### Output
+
+```sql
+INSERT INTO employees (name, age, department, salary, hired_date, manager_id) VALUES ('John', '30', 'IT', '5000.00', '2024-03-20', '1');
+```
+
+### Selecting Data
+
+```rust
+use lumus_sql_builder::sqlite::{Select, Where};
+
+fn main() {
+    let select = Select::new("employees")
+        .columns("name, age, department")
+        .condition(Where::from("age > 25").build())
+        .order("age DESC")
+        .limit(10);
+
+    println!("{}", select.build().unwrap());
+}
+```
+
+### Output
+
+```sql
+SELECT name, age, department FROM employees WHERE age > 25 ORDER BY age DESC LIMIT 10;
 ```
--- src/sqlite.rs
+++ src/sqlite.rs
@@ -463,44 +463,57 @@ impl fmt::Display for Insert {
         }
     }
 }
-
+/// Represents a WHERE clause builder for SQL queries.
 pub struct Where {
     statement: String,
 }
 
 impl Where {
+    /// Creates a new `Where` instance with an empty statement.
     pub fn new() -> Self {
         Self {
             statement: String::new(),
         }
     }
 
+    /// Creates a new `Where` instance with a specified initial statement.
     pub fn from(statement: &str) -> Self {
         Self {
             statement: statement.to_string(),
         }
     }
 
+    /// Adds an equality condition (`field = value`) to the WHERE clause.
     pub fn equal_to(&mut self, field: &str, value: &str) -> &mut Self {
-        self.add_predicate(field, "=", value).unwrap()
+        self.add_predicate(field, "=", value).unwrap();
+        self
     }
 
+    /// Adds a not equal condition (`field != value`) to the WHERE clause.
     pub fn not_equal_to(&mut self, field: &str, value: &str) -> &mut Self {
-        self.add_predicate(field, "!=", value).unwrap()
+        self.add_predicate(field, "!=", value).unwrap();
+        self
     }
 
+    /// Adds a greater than condition (`field > value`) to the WHERE clause.
     pub fn greater_than(&mut self, field: &str, value: &str) -> &mut Self {
-        self.add_predicate(field, ">", value).unwrap()
+        self.add_predicate(field, ">", value).unwrap();
+        self
     }
 
+    /// Adds a greater than or equal condition (`field >= value`) to the WHERE clause.
     pub fn greater_than_equal(&mut self, field: &str, value: &str) -> &mut Self {
-        self.add_predicate(field, ">=", value).unwrap()
+        self.add_predicate(field, ">=", value).unwrap();
+        self
     }
 
+    /// Adds a less than condition (`field < value`) to the WHERE clause.
     pub fn less_than(&mut self, field: &str, value: &str) -> &mut Self {
-        self.add_predicate(field, "<", value).unwrap()
+        self.add_predicate(field, "<", value).unwrap();
+        self
     }
 
+    /// Adds a less than or equal condition (`field <= value`) to the WHERE clause.
     pub fn less_than_equal(
         &mut self,
         field: &str,
@@ -509,56 +522,74 @@ impl Where {
         self.add_predicate(field, "<=", value)
     }
 
+    /// Adds a `IS NULL` condition (`field IS NULL`) to the WHERE clause.
     pub fn is_null(&mut self, field: &str) -> &mut Self {
-        self.add_predicate(field, "IS NULL", "").unwrap()
+        self.add_predicate(field, "IS NULL", "").unwrap();
+        self
     }
 
+    /// Adds a `IS NOT NULL` condition (`field IS NOT NULL`) to the WHERE clause.
     pub fn is_not_null(&mut self, field: &str) -> &mut Self {
-        self.add_predicate(field, "IS NOT NULL", "").unwrap()
+        self.add_predicate(field, "IS NOT NULL", "").unwrap();
+        self
     }
 
-    pub fn inside(&mut self, field: &str, fields: Vec<&str>) -> &mut Self {
-        self.add_predicate(field, "IN", &format!("({})", fields.join(", ")))
-            .unwrap()
+    /// Adds an `IN` condition (`field IN (values)`) to the WHERE clause.
+    pub fn inside(&mut self, field: &str, values: Vec<&str>) -> &mut Self {
+        self.add_predicate(field, "IN", &format!("({})", values.join(", ")))
+            .unwrap();
+        self
     }
 
-    pub fn not_inside(&mut self, field: &str, fields: Vec<&str>) -> &mut Self {
-        self.add_predicate(field, "NOT IN", &format!("({})", fields.join(", ")))
-            .unwrap()
+    /// Adds a `NOT IN` condition (`field NOT IN (values)`) to the WHERE clause.
+    pub fn not_inside(&mut self, field: &str, values: Vec<&str>) -> &mut Self {
+        self.add_predicate(field, "NOT IN", &format!("({})", values.join(", ")))
+            .unwrap();
+        self
     }
 
+    /// Adds a `LIKE` condition (`field LIKE value`) to the WHERE clause.
     pub fn like(&mut self, field: &str, value: &str) -> &mut Self {
-        self.add_predicate(field, "LIKE", value).unwrap()
+        self.add_predicate(field, "LIKE", value).unwrap();
+        self
     }
 
+    /// Adds a `NOT LIKE` condition (`field NOT LIKE value`) to the WHERE clause.
     pub fn not_like(&mut self, field: &str, value: &str) -> &mut Self {
-        self.add_predicate(field, "NOT LIKE", value).unwrap()
+        self.add_predicate(field, "NOT LIKE", value).unwrap();
+        self
     }
 
+    /// Appends `AND` to the current statement in the WHERE clause.
     pub fn and(&mut self) -> &mut Self {
         self.statement.push_str("AND ");
         self
     }
 
+    /// Appends `OR` to the current statement in the WHERE clause.
     pub fn or(&mut self) -> &mut Self {
         self.statement.push_str("OR ");
         self
     }
 
+    /// Appends a left parenthesis `(` to the current statement in the WHERE clause.
     pub fn nest(&mut self) -> &mut Self {
         self.statement.push('(');
         self
     }
 
+    /// Appends a right parenthesis `)` to the current statement in the WHERE clause.
     pub fn unnest(&mut self) -> &mut Self {
         self.statement.push(')');
         self
     }
 
+    /// Constructs and returns the final SQL statement represented by the WHERE clause.
     pub fn build(&self) -> String {
         self.statement.trim().to_string()
     }
 
+    /// Internal method to add a predicate (`field predicate value`) to the WHERE clause.
     fn add_predicate(
         &mut self,
         field: &str,
@@ -584,5 +615,12 @@ impl Where {
         self.statement
             .push_str(&format!("{} {} {} ", field, predicate, escaped_value));
         Ok(self)
+    }
+}
+
+/// Implementation of the Display trait for `Where`, allowing it to be printed.
+impl fmt::Display for Where {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.build())
     }
 }
